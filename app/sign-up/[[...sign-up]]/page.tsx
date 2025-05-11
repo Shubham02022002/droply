00d518867effb652952b6660e1d9950c8347bb2a
Shubham02022002 <chaudhary2001shubham@gmail.com>
2025-05-11 19:51:43 +0530

    backend

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import SignInForm from '@/components/SignInForm'
+import React from 'react'
+
+function SignUpPage() {
+    return (
+        <div>
+            <SignInForm />
+        </div>
+    )
+}
+
+export default SignUpPage
